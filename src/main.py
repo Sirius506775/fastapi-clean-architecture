4ae4871fb0857768f01f64037582b98f6b3d25bd
Dong Heon Lee <Sirius506775@gmail.com>
2024-10-30 14:32:50 +0900

    [feat] : 유효성 검사 오류 422 상태코드를 400으로 변경하여 처리하는 에러 핸들러 등록

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException
 
 import uvicorn
 
@@ -6,6 +10,15 @@ from user.interface.controllers.user_controller import router as user_routers
 
 app = FastAPI()
 app.include_router(user_routers)
+
+
+# fastapi에서 400 에러를 422로 처리하고 있기 때문에 , 예외 핸들러를 등록하여 응답코드를 400으로 변경
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=400,
+        content={"detail": exc.errors(), "body": exc.body},
+    )
 
 
 @app.get("/")
